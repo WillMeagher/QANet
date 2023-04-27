cb0ca2da4017d5fdabc2f0189ae90d9325ef0fe0
AR615598 <66263198+AR615598@users.noreply.github.com>
2023-04-27 00:45:06 -0400

    Added guesser, buzzer, util. Not much implemntation

--- qbmodel.py
+++ qbmodel.py
@@ -2,11 +2,42 @@ from typing import List, Tuple
 import torch as th
 import nltk
 import sklearn
-import transformers
 import numpy as np
 import pandas as pd
 
+"""
+Our plan is to use a RNN to predict the answers to questions.
+ We plan to either use the LSTM model or some common variation on 
+ it. In addition, we hope to add inputs to the model that are the 
+ results of a search engine query about parts of the question. 
+ This means that after a question is input the model will perform 
+ some searches to gain more information about the topics in the 
+ question and hopefully come to a more informed answer. 
 
+Some techniques we are considering attempting to implement 
+regarding using a search engine include using top results as 
+a bag of words feature and inputting top results word by word 
+into the RNN.
+
+Other techniques that we may use include word embeddings such as 
+word2vec, part-of-speech tagging, and tf-idf when creating 
+features/inputs to our RNN. We will also be required to use 
+the techniques associated with training a network such as an RNN. 
+
+We will look into having different output types from our RNN 
+such as a vector that represents Wikipedia pages, letting 
+the model output word embeddings, and having an output that 
+represents confidence which will allow the model to decide 
+when to buzz. 
+
+
+What needs to be done:
+- First lets make the model work with the whole question, later we can implement runs.
+- Quanta can be split into two parts, the guessor and buzzer.
+- The guessor generates the guess and the buzzer decides when to buzz.
+- We should also have a evaluator, but that can be done later.
+
+"""
 class QuizBowlModel:
 
     def __init__(self):
